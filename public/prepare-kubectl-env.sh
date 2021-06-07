c27149b87a91624b2637f56549236453279825f1
devops.maksim.p <devops.maksim.p@alldigitalads.com>
2021-06-07 09:32:56 +0300

    ident

--- public/prepare-kubectl-env.sh
+++ public/prepare-kubectl-env.sh
@@ -7,11 +7,8 @@ set linenumbers
 set tabstospaces
 set tabsize 2
 EOF
-
 export EDITOR=nano
 export do="--dry-run=client -o yaml"
-
 source <(kubectl completion bash)
-
 alias k=kubectl
 complete -F __start_kubectl k
