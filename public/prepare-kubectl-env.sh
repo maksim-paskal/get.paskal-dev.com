1dd7440d007a905e4d73705e71667a7eaca39ce9
devops.maksim.p <devops.maksim.p@alldigitalads.com>
2021-06-07 09:44:03 +0300

    add shortcuts

--- public/prepare-kubectl-env.sh
+++ public/prepare-kubectl-env.sh
@@ -12,3 +12,6 @@ export do="--dry-run=client -o yaml"
 source <(kubectl completion bash)
 alias k=kubectl
 complete -F __start_kubectl k
+alias k-get-pods="kubectl get pods -A"
+alias k-test="kubectl apply --dry-run=client --validate -f "
+alias k-run-curl="kubectl run test -it --rm --image=nginx:alpine --restart=Never -- curl --max-time 5 "
